9ef8bb49f5014e85fc0535a0fcbae2f3d626aefe
kawai norimitsu <kawai.norimitsu@gmail.com>
2015-10-24 15:13:33 +0900

    First commit.

---
+++ sample01-04/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  sample01-04
+//
+//  Created by 河合 徳光 on 2015/10/24.
+//  Copyright © 2015年 norimitsu kawai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func nextBtnPushed(sender: AnyObject) {
+        tabBarController?.selectedViewController = tabBarController?.childViewControllers[1]
+    }
+
+}
+
